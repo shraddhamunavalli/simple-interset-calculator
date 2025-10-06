09c70f6e519deabeb1621a3323115dd6188f5c5b
shraddhamunavalli <munavallishraddha4@gmail.com>
2025-10-06 15:49:03 +0530

    Create calculator.py

---
+++ calculator.py
@@ -0,0 +1,14 @@
+# Simple and Compound Interest Calculator
+
+# Input values
+P = float(input("Enter the principal amount: "))
+R = float(input("Enter the rate of interest: "))
+T = float(input("Enter the time (in years): "))
+
+# Calculate Simple Interest
+SI = (P * R * T) / 100
+print("Simple Interest: ₹", round(SI, 2))
+
+A = P * (1 + R / 100) ** T
+CI = A - P
+print("Compound Interest: ₹", round(CI, 2))
